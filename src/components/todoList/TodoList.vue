b393d15c5038a1ecac019e825b440cfd0bed4444
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-11-24 14:59:48 +0100

    Ajout d'un Item

--- src/components/todoList/TodoList.vue
+++ src/components/todoList/TodoList.vue
@@ -15,6 +15,12 @@ onMounted(async () => {
   DB.setApiURL(props.apiURL);
   todos.splice(todos.length, 0, ...(await DB.findAll()));
 });
+
+// FONCTIONS CRUD
+const createItem = async (content) => {
+  const todo = await DB.create(content);
+  todos.push(todo);
+};
 </script>
 
 <template>
@@ -26,7 +32,7 @@ onMounted(async () => {
     <h2 id="todo-heading" class="sr-only">Todo list</h2>
 
     <!-- INPUT PRINCIPAL -->
-    <TodoListAddForm />
+    <TodoListAddForm @on-submit-add-form="createItem($event)" />
 
     <!-- LISTE DES TODOS -->
     <ul
--- src/components/todoList/TodoListAddForm.vue
+++ src/components/todoList/TodoListAddForm.vue
@@ -1,14 +1,29 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const emits = defineEmits(["onSubmitAddForm"]);
+
+const newContent = ref("");
+
+const onSubmitAddForm = () => {
+  emits("onSubmitAddForm", newContent.value);
+  newContent.value = "";
+};
+</script>
 <template>
   <!-- INPUT PRINCIPAL -->
   <div class="border-b border-slate-200 bg-slate-50 py-3 px-4 sm:p-5">
-    <form class="flex gap-2" aria-label="Add a new task">
+    <form
+      class="flex gap-2"
+      aria-label="Add a new task"
+      @submit.prevent="onSubmitAddForm()"
+    >
       <label for="new-todo" class="sr-only">New task</label>
       <input
         id="new-todo"
         type="text"
         placeholder="What needs to be done?"
-        class="w-full bg-slate-100 border border-slate-300 px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        class="w-full text-slate-600 bg-slate-100 border border-slate-300 px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        v-model="newContent"
       />
       <button
         type="submit"
